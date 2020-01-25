18a5c130970a6c45873d2388b87d187fc9d85fe3
rizkynajmuddin <60282088+rizkynajmuddin@users.noreply.github.com>
2020-01-25 16:19:08 +0700

    Update Nomor2.js

--- Nomor2.js
+++ Nomor2.js
@@ -3,8 +3,23 @@ function isAcceptedUsername(username) {
     return Regex.test(username) ;
 }
 // Cara menggunakan fungsi di atas
-if (isAcceptedUsername("coba1@yuk")) {
+if (isAcceptedUsername("cobain_yuk")) {
     alert("Username Is Valid") ;
 } else {
     alert("Username Is Invalid") ;
+}
+
+function validatePassword() {
+    var myPassword = document.getElementById('password').value;
+    var regex  = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^\&*\)\(+=._-]).+$/;
+    console.log(myPassword);
+    if(myPassword.length <=7){
+        alert("passwordmu kurang dari 9 karakter"); 
+        return false;
+    }else if(!regex.test(myPassword)) {
+        alert("passwordmu harus terdapat sebuah huruf kecil, sebuah angka dan sebuah karakter spesial");
+        return false;
+    }else{
+        alert("passwordmu valid");
+    }
 }
